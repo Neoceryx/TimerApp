36e1a2eb32d0dd5949032ebd6dddc5b239a10aef
Neoceryx <dani.fierro796@gmail.com>
2021-04-06 00:32:42 -0700

    Timer funcionality has been improved

--- agularApp/src/app/timer/timer.component.ts
+++ agularApp/src/app/timer/timer.component.ts
@@ -9,8 +9,8 @@ export class TimerComponent implements OnInit {
 
   constructor() { }
 
-  Hours:number = 0;
-  Minutes:number = 2;
+  Hours:number = 1;
+  Minutes:number = 0;
   Seconds:number = 0;
 
   Timer:any = null;
@@ -22,7 +22,7 @@ export class TimerComponent implements OnInit {
 
   async StartTimer(){
 
-    this.IsRunning = true;  
+    this.IsRunning = true; 
 
      this.Timer = setInterval(() => {
        this.StartDecrement();
@@ -47,25 +47,33 @@ export class TimerComponent implements OnInit {
 
   async StartDecrement(){
 
+    var HoursToSec = this.Hours * 3600;
+    var MinToSec = this.Minutes * 60;
+    var TotalSec = 0;
     
-    this.Seconds--;
-    
-    if (this.Seconds <= 0 && this.Minutes <= 0 && this.Hours <=0) {
-      await this.StopTimer();
-      this.Sound.play();        
+    if (this.Hours == 0 && this.Minutes == 59  && this.Seconds == 59) {
+      TotalSec = HoursToSec + MinToSec;
     }else{
+      TotalSec = HoursToSec + MinToSec + this.Seconds;
+    }
+    
+    var TotalSecondsRemaining = TotalSec - 1;    
+    var SecToHours = TotalSecondsRemaining / 3600 ;
+    var MinToSec = (SecToHours % 1) * 60;
+    var SecondsRemaining = (MinToSec % 1) * 60
 
-      if (this.Seconds <= -1) { 
+    var min = MinToSec.toString().split(".");
+    var hours = SecToHours.toString().split(".")
 
-        this.Seconds = 59;
-        this.Minutes --;
-  
-        if (this.Minutes <= 0 && this.Hours <= 0) {
-          this.Minutes = 0;
-        }
-          
-      }
-
+    this.Hours = parseInt(hours[0]);
+    this.Minutes = parseInt(min[0]);
+    this.Seconds = Math.round(SecondsRemaining);
+    
+    if (TotalSecondsRemaining <= 0) {
+     
+      await this.StopTimer();
+      this.Sound.play();   
+   
     }
 
   }
